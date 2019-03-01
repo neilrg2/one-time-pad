7c0ccdf782811fd697b301421f95d6c815d7eb87
Neil Gayeta <neilrg2@gmail.com>
2019-02-28 17:37:59 -0800

    keygen comments

--- otp/otp/keygen.c
+++ otp/otp/keygen.c
@@ -13,24 +13,36 @@ int main(int argc, const char *argv[])
     
     char *key_ptr;
     
-    int key_size = (int)strtol(argv[1], &key_ptr, 10);
+    int key_size = (int)strtol(argv[1], &key_ptr, 10); /* Convert second argument to long with int cast */
     int i, random_value, file_descriptor;
     
     char generated_key[key_size + 1];
     
+    
+    /* Generate random values from the allowed 27 characters and store into string */
     for (i = 0; i < key_size; i++)
     {
         random_value = rand() % (91 - 32) + 32;
+        
+        /* Follows ASCII values of characters (65-90 capital letters) and 32 is the space character;
+          Continue to loop until the integer returned from rand is either 32 or a value between
+          65 and 90 */
         while (random_value != 32 && (random_value < 65 || random_value > 90))
         {
             random_value = rand() % (91 - 32) + 32;
         }
         generated_key[i] = random_value;
     }
-    generated_key[key_size] = '\n';
+    generated_key[key_size] = '\n'; /* Append last character as a newline */
     
+    
+    /* Checks if there is an argument at index 2 and if there is, if that argument
+       is a stdout redirection character */
     if (argv[2] && !strcmp(argv[2], ">"))
     {
+        
+        /* Opens file for redirection specified as command line argument at index 3;
+          dup2 call for stdout redirection to specified file */
         if (argc == 4 && argv[3])
         {
             file_descriptor = open(argv[3], O_CREAT | O_TRUNC | O_RDWR, 0755);
@@ -49,8 +61,11 @@ int main(int argc, const char *argv[])
         }
     }
     
+    /* If file redirection occurred, the randomly generated string will be written
+       to the specified file. Else, the string will be read to the screen as normal
+       use of stdout */
     write(STDOUT_FILENO, generated_key, key_size + 1);
-    
+    close(STDOUT_FILENO);
     return 0;
 }
 
